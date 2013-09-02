fd4a5ac5f93153f069244f9c79138c0f4cac210a
Adam Wright <adamwright@cmu.edu>
2013-09-02 08:32:46 -0400

    Working echo server

--- echoserver.rs
+++ echoserver.rs
@@ -1,10 +1,46 @@
-fn copy_stream(in : TcpStream, out : TcpStream) {
-}
+extern mod extra;
 
-fn handle_client(client_stream : TcpStream) {
-    copy_stream(client_stream, client_stream);
+use std::os::args;
+use std::cell::Cell;
+use std::rt::io::{Reader, Writer, Listener};
+use std::rt::io::net::ip::SocketAddr;
+use std::rt::io::net::tcp::{TcpListener};
+
+use extra::getopts::{getopts, reqopt, opt_str, fail_str};
+
+fn echo<S : Reader + Writer>(stream: &mut S) {
+    let mut buf = [0, ..1024];
+    loop {
+        match stream.read(buf) {
+            Some(n) => stream.write(buf.slice_to(n)),
+            None => break,
+        }
+    }
 }
 
 fn main() {
-    println("sup dawg")
+    let args = args();
+    let opts = [reqopt("addr")];
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => m,
+        Err(f) => fail!(fail_str(f)),
+    };
+    let saddr = opt_str(&matches, "addr");
+    let addr : SocketAddr = FromStr::from_str(saddr)
+        .expect("invalid address: " + saddr);
+    let mut l = TcpListener::bind(addr)
+        .expect(format!("failed to listen on socket {}", saddr));
+
+    loop {
+        match l.accept() {
+            Some(client) => {
+                let client = Cell::new(client);
+                do spawn {
+                    let mut client = client.take();
+                    echo(&mut client)
+                }
+            },
+            None => println("error in accept"),
+        }
+    }
 }
