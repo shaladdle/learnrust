c4b989ab9c300b56ae9f9959a2a1b5e4a17dc2df
Adam Wright <adamwright@cmu.edu>
2013-09-02 07:27:45 -0400

    Added copy_stream

--- echoserver.rs
+++ echoserver.rs
@@ -1,4 +1,8 @@
-fn handle_client() {
+fn copy_stream(in : TcpStream, out : TcpStream) {
+}
+
+fn handle_client(client_stream : TcpStream) {
+    copy_stream(client_stream, client_stream);
 }
 
 fn main() {
