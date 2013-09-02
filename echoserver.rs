90997bd842a20379e6547ae73e335d1136fee5b0
Adam Wright <adamwright@cmu.edu>
2013-09-02 08:42:27 -0400

    Got transfer working

--- echoserver.rs
+++ echoserver.rs
@@ -8,11 +8,11 @@ use std::rt::io::net::tcp::{TcpListener};
 
 use extra::getopts::{getopts, reqopt, opt_str, fail_str};
 
-fn echo<S : Reader + Writer>(mut stream: S) {
+fn transfer<R : Reader, W : Writer>(input: @mut R, output: @mut W) {
     let mut buf = [0, ..1024];
     loop {
-        match stream.read(buf) {
-            Some(n) => stream.write(buf.slice_to(n)),
+        match input.read(buf) {
+            Some(n) => output.write(buf.slice_to(n)),
             None => break,
         }
     }
@@ -36,8 +36,8 @@ fn main() {
             Some(client) => {
                 let client = Cell::new(client);
                 do spawn {
-                    let client = client.take();
-                    echo(client)
+                    let client = @mut client.take();
+                    transfer(client, client)
                 }
             },
             None => println("error in accept"),
